ea6f26973b99b47958b604b4803ceb5bbf76d5b0
Kat <kyevsukov@gmail.com>
2021-12-03 14:00:03 -0500

    can get all actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,8 +1,14 @@
 // Write your "actions" router here!
 const router = require('express').Router()
+const Action = require('./actions-model')
 
-router.get('/', (req, res, next) => {
-    res.send('this is just a test')
+router.get('/', async (req, res, next) => {
+    try {
+        const actions = await Action.get()
+        res.status(200).json(actions)
+    } catch(err) {
+        next(err)
+    }
 })
 
 module.exports = router
