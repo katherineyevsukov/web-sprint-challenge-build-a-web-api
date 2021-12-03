fe152dd121db0e00d387ab8183e11420f979ca87
Kat <kyevsukov@gmail.com>
2021-12-03 15:03:34 -0500

    cleaned up

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,22 +1,4 @@
-// add middlewares here related to projects
 const Project = require("./projects-model");
-
-// async function checkId(req, res, next) {
-//     const { id } = req.params;
-//     try {
-//       const proj = await Project.get(id);
-//       if (!proj) {
-//         res
-//           .status(404)
-//           .json({ message: `sorry, no project with id ${id} was found` });
-//       } else {
-//         req.projFromDb = proj;
-//         next();
-//       }
-//     } catch (err) {
-//       next(err);
-//     }
-//   }
 
 async function checkId(req, res, next) {
   const { id } = req.params;
--- api/server.js
+++ api/server.js
@@ -7,10 +7,6 @@ server.use(express.json())
 server.use('/api/projects', projectsRouter)
 server.use('/api/actions', actionsRouter)
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
 
 server.get('/', (req, res) => {
     res.send('server up and running!')
