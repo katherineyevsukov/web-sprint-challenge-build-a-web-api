a375e1154c8355cdffa28418dc22e7f010eb0c60
Kat <kyevsukov@gmail.com>
2021-12-03 12:51:54 -0500

    can get all projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,5 +2,13 @@ const express = require('express')
 const Project = require('./projects-model')
 const router = express.Router()
 
+router.get('/', async (req, res, next) => {
+    try {
+        const projects = await Project.get()
+        res.status(200).json(projects)
+    } catch(err){
+        next(err)
+    }
+})
 
 module.exports = router
