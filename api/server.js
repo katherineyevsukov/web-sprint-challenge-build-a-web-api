9dbe40ee93701da4ae33d4bdbbdcdd13459cbd2d
Kat <kyevsukov@gmail.com>
2021-12-03 12:41:41 -0500

    added error handling and 404 middleware

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json())
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
@@ -8,6 +10,14 @@ const server = express();
 
 server.get('/', (req, res) => {
     res.send('server up and running!')
+})
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` })
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({message: process.env.NODE_ENV === 'PRODUCTION' ? 'sorry, there was an erorr' : err.message})
 })
 
 module.exports = server;
