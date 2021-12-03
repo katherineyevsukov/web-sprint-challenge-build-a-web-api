d6017b7fe6c4a2449d17b42a5811bd1e13b9c9c4
Kat <kyevsukov@gmail.com>
2021-12-03 13:56:12 -0500

    actions router is connected

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,8 @@
 // Write your "actions" router here!
+const router = require('express').Router()
+
+router.get('/', (req, res, next) => {
+    res.send('this is just a test')
+})
+
+module.exports = router
