07b8409af1896cc92a161fa05aaa42cb7484cc52
Kat <kyevsukov@gmail.com>
2021-12-03 13:03:29 -0500

    checkid middleware is working

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,9 +1,36 @@
 // add middlewares here related to projects
-const Project = require('./projects-model')
+const Project = require("./projects-model");
 
-async function checkId(req, res, next){
- console.log('sup')
- next()
+// async function checkId(req, res, next) {
+//     const { id } = req.params;
+//     try {
+//       const proj = await Project.get(id);
+//       if (!proj) {
+//         res
+//           .status(404)
+//           .json({ message: `sorry, no project with id ${id} was found` });
+//       } else {
+//         req.projFromDb = proj;
+//         next();
+//       }
+//     } catch (err) {
+//       next(err);
+//     }
+//   }
+
+async function checkId(req, res, next) {
+  const { id } = req.params;
+  try {
+    const proj = await Project.get(id);
+    !proj
+      ? res
+          .status(404)
+          .json({ message: `sorry, no project with id ${id} was found` })
+      : (req.projFromDb = proj);
+    next();
+  } catch (err) {
+    next(err);
+  }
 }
 
-module.exports = { checkId, }
+module.exports = { checkId };
