f5603d9c45f90e932bc76fe790b73a249f7d6196
Kat <kyevsukov@gmail.com>
2021-12-03 13:10:21 -0500

    can get project by id

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const { checkId } = require('./projects-middleware')
-const Project = require('./projects-model')
-const router = express.Router()
+const express = require("express");
+const { checkId } = require("./projects-middleware");
+const Project = require("./projects-model");
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-    try {
-        const projects = await Project.get()
-        res.status(200).json(projects)
-    } catch(err){
-        next(err)
-    }
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const projects = await Project.get();
+    res.status(200).json(projects);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get('/:id', checkId, (req, res, next) => {
-    console.log('hey')
-})
-module.exports = router
+router.get("/:id", checkId, (req, res, next) => {
+  res.status(200).json(req.projFromDb)
+});
+module.exports = router;
