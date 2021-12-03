cc99ea39d38948e907197134e91769a20be0328f
Kat <kyevsukov@gmail.com>
2021-12-03 14:07:29 -0500

    can get action by id

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,16 @@
-// add middlewares here related to actions
+const Action = require("./actions-model");
+
+async function checkId(req, res, next) {
+  const { id } = req.params;
+  try {
+    const action = await Action.get(id);
+    !action
+      ? next({ status: 404, message: `action with id ${id} was not found` })
+      : (req.actionFromDb = action);
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = { checkId };
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,14 +1,19 @@
 // Write your "actions" router here!
-const router = require('express').Router()
-const Action = require('./actions-model')
+const router = require("express").Router();
+const { checkId } = require("./actions-middlware");
+const Action = require("./actions-model");
 
-router.get('/', async (req, res, next) => {
-    try {
-        const actions = await Action.get()
-        res.status(200).json(actions)
-    } catch(err) {
-        next(err)
-    }
+router.get("/", async (req, res, next) => {
+  try {
+    const actions = await Action.get();
+    res.status(200).json(actions);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', checkId, async (req, res, next) => {
+    res.status(200).json(req.actionFromDb)
 })
 
-module.exports = router
+module.exports = router;
